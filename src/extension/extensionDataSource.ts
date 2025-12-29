db21a9df95734baf31cce17a498a2d5e8dc0ba88
thaske <10328778+thaske@users.noreply.github.com>
2025-12-28 20:31:27 -0500

    fix: remove unused CalendarData import

--- src/extension/extensionDataSource.ts
+++ src/extension/extensionDataSource.ts
@@ -1,5 +1,5 @@
 import type { DataSource } from "../shared/dataSource";
-import type { CalendarData, CalendarResponse } from "../types";
+import type { CalendarResponse } from "../types";
 
 type RuntimeMessage =
   | ({ type: "calendar_update" } & CalendarResponse)
